8a1b45949f3aef3a51114f7ce96c0f4801e856bd
vo1odko <vova.stonozhenko@mail.ru>
2025-04-29 12:32:52 +0300

    .

--- src/main/java/com/blackblaze/controller/ServiceController.java
+++ src/main/java/com/blackblaze/controller/ServiceController.java
@@ -1,9 +1,12 @@
 package com.blackblaze.controller;
 
+import com.blackblaze.model.ServiceEntity;
 import com.blackblaze.service.ServiceService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class ServiceController {
@@ -13,9 +16,10 @@ public class ServiceController {
         this.serviceService = serviceService;
     }
 
-    @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("services", serviceService.getAllServices());
+    @GetMapping(value = {"", "/", "/index"})
+    public String getIndexPage(Model model) {
+        List<ServiceEntity> services = serviceService.getAllServices();
+        model.addAttribute("services", services);
         return "index";
     }
 }
